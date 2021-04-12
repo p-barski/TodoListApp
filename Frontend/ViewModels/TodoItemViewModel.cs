fc4002fee806b45167cbdc453d5041e8af1cebbc
p-barski <patryk.kubarski@protonmail.com>
2021-04-12 14:00:31 +0200

    Added IsFinished checkbox to TodoItemView

--- Frontend/ViewModels/TodoItemViewModel.cs
+++ Frontend/ViewModels/TodoItemViewModel.cs
@@ -1,3 +1,4 @@
+using ReactiveUI;
 using Backend;
 
 namespace Frontend.ViewModels
@@ -6,15 +7,26 @@ namespace Frontend.ViewModels
 	{
 		public string Date => todoItem.Date.ToString();
 		public string Task => todoItem.Task;
-		public string IsFinished => todoItem.IsFinished.ToString();
+		public bool IsFinished
+		{
+			get => isFinished;
+			set
+			{
+				this.RaiseAndSetIfChanged(ref isFinished, value);
+				todoItem.IsFinished = isFinished;
+				databaseAccess.Update(todoItem);
+			}
+		}
 		public delegate void Deleted(TodoItemViewModel sender);
 		public event Deleted? ItemDeletedEvent;
 		private readonly IDatabaseAccess databaseAccess;
 		private readonly TodoItem todoItem;
+		private bool isFinished;
 		public TodoItemViewModel(IDatabaseAccess databaseAccess, TodoItem todoItem)
 		{
 			this.databaseAccess = databaseAccess;
 			this.todoItem = todoItem;
+			isFinished = todoItem.IsFinished;
 		}
 		private void OnDeleteClick()
 		{
