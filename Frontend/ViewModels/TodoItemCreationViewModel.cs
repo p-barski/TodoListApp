39859687d567d26512f87456d6ab4c5de87fe607
p-barski <patryk.kubarski@protonmail.com>
2021-04-14 20:02:15 +0200

    Creating new todo item resets values in textbox and timepicker.

--- Frontend/ViewModels/TodoItemCreationViewModel.cs
+++ Frontend/ViewModels/TodoItemCreationViewModel.cs
@@ -1,15 +1,31 @@
 using System;
+using ReactiveUI;
 using Backend;
 
 namespace Frontend.ViewModels
 {
 	public class TodoItemCreationViewModel : ViewModelBase
 	{
-		public DateTimeOffset Date { get; set; } = DateTime.Now;
-		public TimeSpan Time { get; set; } = DateTime.Now.TimeOfDay;
-		public string Task { get; set; } = "";
+		public DateTimeOffset Date
+		{
+			get => date;
+			set => this.RaiseAndSetIfChanged(ref date, value.Date);
+		}
+		public TimeSpan Time
+		{
+			get => time;
+			set => this.RaiseAndSetIfChanged(ref time, value);
+		}
+		public string Task
+		{
+			get => task;
+			set => this.RaiseAndSetIfChanged(ref task, value);
+		}
 		public delegate void Created(TodoItem todoItem);
 		public event Created? ItemCreatedEvent;
+		private DateTime date = DateTime.Now;
+		private TimeSpan time = DateTime.Now.TimeOfDay;
+		private string task = "";
 		private readonly IDatabaseAccess databaseAccess;
 		public TodoItemCreationViewModel(IDatabaseAccess databaseAccess)
 		{
@@ -23,6 +39,8 @@ namespace Frontend.ViewModels
 				Task = Task,
 				IsFinished = false
 			};
+			Task = "";
+			Time = DateTime.Now.TimeOfDay;
 			databaseAccess.Add(todoItem);
 			ItemCreatedEvent?.Invoke(todoItem);
 		}
